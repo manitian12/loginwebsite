2de23141b545133c16837a7fee4fe0c6c01048b1
manitian12 <12deori23@gmail.com>
2024-10-28 14:13:29 +0530

    Update otpService.js

--- otpService.js
+++ otpService.js
@@ -1,1 +1,19 @@
+// otpService.js
+const nodemailer = require('nodemailer');
+
+async function sendOTPEmail(email, otp) {
+  let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: { user: process.env.EMAIL, pass: process.env.EMAIL_PASS }
+  });
+
+  await transporter.sendMail({
+    from: process.env.EMAIL,
+    to: email,
+    subject: 'Your OTP Code',
+    text: `Your OTP code is ${otp}.`
+  });
+}
+
+module.exports = { sendOTPEmail };
 
